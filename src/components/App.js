28fbbb0e1c6217a25e3578f43d92893d1e58016c
Erin Hinson <erin.hinson@XCELC02Q33V.local>
2017-06-27 17:47:22 -0400

    Initial Commit

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import data from '../data.json';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Welcome to React</h2>
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default App;
