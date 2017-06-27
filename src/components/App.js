158dd7c2b6da01d0e169fb1eb4eeeb9cd7254d8b
Erin Hinson <erin.hinson@XCELC02Q33V.local>
2017-06-27 18:02:08 -0400

    Flex table

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
 import data from '../data.json';
+import { Table, TableCell } from '../styled-components/Table';
 
 class App extends Component {
+  loopTableCells = (min, max, iterator) => {
+    var table = [];
+    for (var i = min; i <= max; i++) {
+      table.push(i);
+    }
+    if (table.length % 5 !== 0) {
+      let gray = Array(5 - table.length % 5);
+      table.push(...gray);
+    }
+    return table;
+  };
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h2>Welcome to React</h2>
+        </div>
+        <div>
+          <Table width={data.red.width}>
+            {this.loopTableCells(data.red.min, data.red.max, data.red.increment).map(number => (
+              <TableCell key={number} className={`table-cell ${!number && 'gray'}`}>
+                {number ? number : 'gray'}
+              </TableCell>
+            ))}
+
+          </Table>
+          <button>Button</button>
+          <button>Button</button>
         </div>
 
       </div>
---
+++ src/styled-components/Table.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Table = styled.div`
+  display: flex;
+  width: 20%;
+  border: 1px solid #eeeeee;
+  flex-direction: row;
+  flex-wrap: wrap-reverse;
+  box-sizing: border-box;
+`;
+
+export const TableCell = styled.div`
+  width: 20%;
+  border: 1px solid gray;
+  box-sizing: border-box;
+`;
